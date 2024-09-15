5220f118963adacddbdf27fe7b657ffc5e3040a1
Vivek <vivekyadav7272@gmail.com>
2024-09-15 14:12:35 +0530

    added width() method; made Mask::new const; Masks can now be d together

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -10,6 +10,7 @@ pub mod mappings {
     }
 }
 pub mod reg_io;
+
 // This is the default clock freq for Arduino Uno. Someone _CAN_ change it if they want to,
 // by messing with the fuse bits. Not handling that right now, maybe later.
 pub const F_CPU: u32 = 16_000_000;
