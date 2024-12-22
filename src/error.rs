40ec214f16e334b9c05742b76355d8fcf495e7f5
Vivek <vivekyadav7272@gmail.com>
2024-12-22 19:11:14 +0530

    initial workings of mutex impl

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,7 @@ use core2::io;
 pub enum Error {
     SingletonAlreadyTaken,
     SerialWriteFailed(io::Error),
+    LockAlreadyAcquiredError,
 }
 
 impl core::error::Error for Error {}
@@ -12,11 +13,12 @@ impl core2::error::Error for Error {}
 impl core::fmt::Display for Error {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
-            Self::SingletonAlreadyTaken => write!(
+            Self::SingletonAlreadyTaken => writeln!(
                 f,
                 "The singleton you're trying to acquire is already taken."
             ),
-            Self::SerialWriteFailed(e) => write!(f, "Serial::write(_) has failed. Error: {e}"),
+            Self::SerialWriteFailed(e) => writeln!(f, "Serial::write(_) has failed. Error: {e}"),
+            Self::LockAlreadyAcquiredError => writeln!(f, "Lock already taken by another entity."),
         }
     }
 }
--- src/sync/mutex.rs
+++ src/sync/mutex.rs
@@ -1,1 +1,54 @@
+use crate::error::Error;
+use crate::sys::interrupt::CriticalSectionGuard;
+use core::marker::PhantomData;
+use core::ptr;
+use core::result::Result;
+use core::{cell::UnsafeCell, ops::Deref};
+
 // TODO: Also implement "Sync" RefCell, which I now realise is just a Mutex.
+pub struct Mutex<T> {
+    // INVARIANT: DO NOT touch this field UNTIL YOU ARE IN A CRITICAL SECTION
+    already_acquired: UnsafeCell<bool>,
+    data: UnsafeCell<T>,
+}
+// unsafe impl<T: Send> Sync for Mutex<T> {}
+
+impl<T> Mutex<T> {
+    pub fn new(data: T) -> Self {
+        Mutex {
+            already_acquired: UnsafeCell::new(false),
+            data: UnsafeCell::new(data),
+        }
+    }
+
+    pub fn lock(&self) -> Result<MutexGuard<T>, Error> {
+        let _guard = CriticalSectionGuard::new();
+        // SAFETY: In critical section, so we currently are the only readers.
+        let already_acquired = unsafe { ptr::read(self.already_acquired.get()) };
+        if already_acquired {
+            // I assume _guard is gonna be dropped at this line right?
+            return Err(Error::LockAlreadyAcquiredError);
+        }
+        // else, we're gonna get the lock on it.
+        // SAFETY: In critical section, so we currently are the only writer.
+        unsafe {
+            ptr::write(self.already_acquired.get(), true);
+        }
+
+        Ok(MutexGuard {
+            guarded_mutex: self,
+            cs_guard: _guard,
+        })
+    }
+}
+
+pub struct MutexGuard<'a, T> {
+    guarded_mutex: &'a Mutex<T>,
+    // Two choices here: Either let the interrupts
+    // come in, check that already_acquired is true, and then do, whatever the f they want.
+    // That is _kinda_ away from the spirit of Mutexes as we experience them on hosted systems,
+    // where if u can't get the lock, u just wait.
+    // So I'd like for interrupts to just not be a thing, while the lock is held.
+    // and then they are "awakened", after the lock is released.
+    cs_guard: CriticalSectionGuard,
+}
